e7aaf09815aae8c0bd287a7855e452564cc8a4a0
Rob Marano <rob@thehackerati.com>
2015-02-28 10:51:35 -0500

    Parameterized the scripts to configure every configuration variable via script/config.sh

--- script/config.sh
+++ script/config.sh
@@ -1,9 +1,40 @@
 #!/bin/bash
-
-export NUMSLAVES=3
+###############################################################################
+# The MIT License (MIT)
+#
+# Copyright (c) 2015 The Hackerati, Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+###############################################################################
 
 export AWS_ACCESS_KEY_ID=""
 export AWS_SECRET_ACCESS_KEY=""
+
 export AWSPROFILE="default"
+
 export MYKEYPAIR=""
-export MYKEYPAIRFILE="`pwd`/$MYKEYPAIR.pem"
+export MYKEYPAIRFILE="`pwd`/${MYKEYPAIR}.pem"
+
+export SPARK_HOME=""
+export SPARK_CLUSTER_NAME="hackademy"
+export NUMSLAVES=3
+export AWS_REGION="us-east-1"
+export AWS_ZONE="us-east-1a"
+export INSTANCE_TYPE="c3.2xlarge"
--- script/createHackademySparkCluster.sh
+++ script/createHackademySparkCluster.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
+###############################################################################
+# The MIT License (MIT)
+#
+# Copyright (c) 2015 The Hackerati, Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+###############################################################################
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $DIR
 
 source config.sh
-cd ../spark/ec2
+cd ${SPARK_HOME}/spark/ec2
 
-./spark-ec2 --key-pair=$MYKEYPAIR --identity-file=$MYKEYPAIRFILE --region=us-east-1 --zone=us-east-1a --copy-aws-credentials --instance-type=c3.2xlarge --slaves=$NUMSLAVES launch hackademy
+./spark-ec2 --key-pair=${MYKEYPAIR} --identity-file=${MYKEYPAIRFILE} --region=${AWS_REGION} --zone=${AWS_ZONE} --copy-aws-credentials --instance-type=${INSTANCE_TYPE} --slaves=${NUMSLAVES} launch ${SPARK_CLUSTER_NAME}
